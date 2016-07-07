f633ff5afd55ce27c4f17ff32846da35f1640627
ibbecknell <missionbit@Missions-MacBook-Air-9.local>
2016-07-07 14:43:35 -0700

    added required functionality
    -can jump up to top ledge with spacebar
    -can move down to bottom ledge with down arrow
    
    You should:
    -add obstacles
    -figure out what happens when you reach the end of the game: Game Over
    or go to next level or start over

--- gameDev/state0.js
+++ gameDev/state0.js
@@ -2,8 +2,9 @@ var demo = {};
 var cursors;
 var square;
 var speed = 3;
-var obstacle;
-var platforms;
+var obstacles;
+var ledge1;
+var ledge2;
 
 var spacekey;
 demo.state0 = function(){};
@@ -11,8 +12,8 @@ demo.state0 = function(){};
 demo.state0.prototype = {
     
     preload: function(){
-       game.load.spritesheet('square', 'assets/spritesheets/WalkingSquare.png', 120, 155);
-       game.load.spritesheet('ledge', 'assets/spritesheets/ledge.png', 1250, 15);
+        game.load.spritesheet('square', 'assets/spritesheets/WalkingSquare.png', 94, 76);
+        game.load.spritesheet('ledge', 'assets/spritesheets/ledge.png', 1250, 15);
         
         game.load.spritesheet('obstacle', 'assets/spritesheets/ledge2.png', 100, 60);
         
@@ -21,81 +22,84 @@ demo.state0.prototype = {
     },
     
     create: function(){
-        
-        game.world.setBounds(0,0,1265,10)
-        
-        game.stage.backgroundColor = '#ffffff';
-//        ledge3 = game.add.sprite(0, 570, 'ledge3');
-        
-        square = game.add.sprite(0, 0, 'square'); 
-         
-        cursors = game.input.keyboard.createCursorKeys();
-//        ledge2 = game.add.sprite(0, 125, 'ledge2');
-        
-        game.physics.enable(square);
-        
-
-        
-        square.body.gravity.y = 100;
+//        Set up
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.world.setBounds(0,0,2800,10)
+        game.stage.backgroundColor = '#ffffff';
         
-//        game.physics.enable(ledge);
+//        Square
+        square = game.add.sprite(0, 480, 'square'); 
+                 
+        game.physics.enable(square);
+        square.body.gravity.y = 300;
         
-//        game.physics.enable(ledge2);
-        
-//        game.physics.enable(ledge3);
-        
-//        ledge3.body.checkCollision.up = true;
+        square.collideWorldBounds = true;
+        square.animations.add('walk', [0,1,2,3,4], 5.5);
+        game.camera.follow(square);
         
         
-//        ledge2.body.checkCollision.up = true;
+//        Bottom Ledge
+        ledge1 = game.add.sprite(0, 565, 'ledge');
+        ledge1.width = 2800;
         
-        platforms= game.add.group();
-        platforms.enableBody = true;
+        game.physics.enable(ledge1);
+        ledge1.body.immovable = true;
         
-        var ledge = platforms.create(0, 565, 'ledge');
-        ledge.body.immovable = true;
+//        Top Ledge
+        ledge2 = game.add.sprite(0, 125, 'ledge');
+        ledge2.width = 2800;
+        game.physics.enable(ledge2);
+        ledge2.body.immovable = true;        
         
-        var ledge = platforms.create(0, 125, 'ledge');
-        ledge.body.immovable = true;
-        
-        var obstacle = platforms.create(400, 520, 'obstacle');
+//        Obstacles group
+        obstacles = game.add.group();
+        obstacles.enableBody = true;
+
+        obstacle = obstacles.create(400, 520, 'obstacle');
         obstacle.body.immovable = true;
         
+//        Input
+        cursors = game.input.keyboard.createCursorKeys();
         this.spacekey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-           
-   square.animations.add('walk', [0,1,2,], 3.5);
-      
-        
-        
     },
     
     update: function(){
+//        Square movement
+        square.body.velocity.x += 1;
         square.animations.play('walk');
-        game.physics.arcade.collide(platforms, square);
-      
-      if(cursors.right.isDown){
-          square.body.velocity.x = 300;
-      }
-    else if(this.spacekey.isDown){
+
+//        What happens when the square reaches the end of your game
+        if (square.x === game.world.width)
+        {
+//            Game Over text?
+//            Next level?
+//            Start over?
+        }
+        
+//        Collision
+        game.physics.arcade.collide(obstacles, square);
+        game.physics.arcade.collide(ledge1, square);
+        game.physics.arcade.collide(ledge2, square);
+
+//        What happens when I press the spacebar?
+        if(this.spacekey.isDown){
+            square.animations.stop();
+            square.body.velocity.x = 0;
             square.body.velocity.y = -200;
+            square.body.checkCollision.up = false;
+
+        }
+//        What happens when I press the down arrow?
+        else if(cursors.down.isDown){
+            ledge2.body.checkCollision.up = false;
+            square.body.velocity.y = 200;
+            square.body.velocity.x = 0;
         }
         else{
-            square.body.velocity.x = 0;
-            square.animations.stop();
-            square.frame = 0;
-        }
-        if(cursors.up.isDown){
-            square.body.velocity.y = -550;
+            ledge2.body.checkCollision.up = true;
         }
        
     }
-    
-    //render: function(){
-    // game.debug.spriteInfo(square, 20, 32); 
-//}
-//        game.debug.bodyInfo(sprite, 16, 24);
-    
     
 };
 
